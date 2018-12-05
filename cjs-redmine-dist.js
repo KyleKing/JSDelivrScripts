fdfaa6acef2c84f6facde5642c256a63e8ac3f34
Kyle King <KyleKing@users.noreply.github.com>
2018-12-05 09:26:51 -0500

    Add: create shareable text files and shorten code

--- cjs-redmine-dist.js
+++ cjs-redmine-dist.js
@@ -1,5 +1,6 @@
 /*
-1. Install CJS2 for Chrome: https://chrome.google.com/webstore/detail/custom-javascript-for-web/ddbjnfjiigjmcpcpkmhogomapikjbjdk
+1. Install CJS2 for Chrome:
+  https://chrome.google.com/webstore/detail/custom-javascript-for-web/ddbjnfjiigjmcpcpkmhogomapikjbjdk
 2. Go to http://bondra.meso-scale.com/redmine
 3. Open the CJS editor (click the tool bar icon). Click on 'Load External Scripts' and paste:
 
@@ -10,49 +11,8 @@
 */
 
 /*
-// Modify header dimension to minimize GUI disruption
-$( '#top-menu' ).css( 'height', '2.5em' )
-$( '#top-menu' ).css( 'padding', '8px 5px 5px 5px' )
-// Source script file
-$( 'body' ).append( '<script src="https://cdn.jsdelivr.net/gh/KyleKing/JSDelivrScripts@latest/cjs-redmine-dist.js"></script>' )
 
-// Ian - Create Progress Bar
-
-if ( window.location.href.indexOf( 'MM_Procedures_-_Manufacturing_and_Service' ) !== -1 ) {
-  // For tables with 8 columns, store the status text in an array
-  //    Based on: https://stackoverflow.com/a/9579792/3219667
-  const issueStatuses = []
-  $( '.wiki.wiki-page table tr' ).each( function() {
-    const cells = $( this ).find( 'td' )
-    if ( cells.length === 8 )
-      issueStatuses.push( $( cells[1] ).text() )
-  } )
-  // Calculate the current progress
-  const countCompl = issueStatuses.filter( value => value === 'Complete' ).length
-  const progress = Math.floor( countCompl / issueStatuses.length * 100 )
-
-  // Create target for Crel to replace
-  const h6Target = $( $( 'h6' )[0] )
-  h6Target.after( '<div id="progress-crel-target"></div>' )
-
-  // Generate Table with Crel
-  crel( document.querySelector( '#progress-crel-target' ),
-    crel( 'h2', `Progress ${progress}% (${countCompl} Complete of ${issueStatuses.length}):` ),
-    crel( 'div', {'class': 'value', 'style': 'height: 45px'},
-      crel( 'table', {'class': `progress progress-${progress}`, 'style': 'display: inline-table'},
-        crel( 'tbody',
-          crel( 'tr',
-            crel( 'td', {'class': 'closed', 'style': `width: ${progress}%`, 'title': `${progress}%`} ),
-            crel( 'td', {'class': 'todo', 'style': `width: ${100 - progress}%`} )
-          )
-        )
-      )
-    )
-  )
-
-  // Remove original target
-  h6Target.remove()
-}
+<See cjs-custom.js>
 
 */
 
